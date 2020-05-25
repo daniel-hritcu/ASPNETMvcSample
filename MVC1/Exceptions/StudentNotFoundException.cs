cf88a0188897f402a4fa88019c9799c7e7db80d8
Daniel Hritcu <daniel@hritcu.ca>
2020-05-25 11:49:22 -0400

    create StudentNotFoundException

---
+++ MVC1/Exceptions/StudentNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MVC1.Exceptions
+{
+    public class StudentNotFoundException : Exception
+    {
+        public StudentNotFoundException()
+        {
+        }
+
+        public StudentNotFoundException(string message) : base(message)
+        {
+        }
+
+        public StudentNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
